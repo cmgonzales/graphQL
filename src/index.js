6ab5345d77e1007d6928a698d1913861fca243ff
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-09-05 19:46:09 -0700

    added spread operator

--- src/index.js
+++ src/index.js
@@ -167,11 +167,12 @@ const resolvers = {
             if(emailTaken){
                 throw new Error('Email Taken.')
             }
+
+            
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
+
             }
             users.push(user)
 
@@ -186,9 +187,7 @@ const resolvers = {
  
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published
+                ...args
             }
 
             posts.push(post)
@@ -205,9 +204,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
 
             comments.push(comment)
